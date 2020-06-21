27b3fc55bbd8e1a64327fe274d8e0e0ed491b22e
refixshow <adasscieszka@wp.pl>
2020-06-21 23:54:02 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import useSetState from "./useSetState";
 
-function App() {
+const App = () => {
+  const [clicks, setClicks] = useSetState({ value: 0 });
+
+  useEffect(() => {
+    console.log(clicks);
+  }, [clicks]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="left">
+        <h1>ANSWER</h1>
+        <div>
+          <div className="answer">{JSON.stringify(clicks, null, 1)}</div>
+          <button
+            onClick={() => {
+              setClicks({ value: clicks.value + 1 });
+            }}
+          >
+            click
+          </button>
+        </div>
+      </div>
+      <div className="right">
+        <h1>TESTS</h1>
+        <div className="test">
+          <span>{JSON.stringify({ value: 400, x: 2, y: 4 }, null, 2)}</span>
+          <button
+            onClick={() => {
+              setClicks({ value: 400, x: 2, y: 4 });
+            }}
+          >
+            test 1
+          </button>
+        </div>
+        <div className="test">
+          <span>
+            {JSON.stringify(["abc", 24, { john: "john" }, [1, 2, 3]], null, 2)}
+          </span>
+          <button
+            onClick={() => {
+              setClicks(["abc", 24, { john: "john" }, [1, 2, 3]]);
+            }}
+          >
+            test 2
+          </button>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
